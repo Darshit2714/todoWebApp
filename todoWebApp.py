b9f0ab6899c5f3c90a63e92a5e75ae66b4787f72
Darshit2714 <darshit2714@gmail.com>
2025-12-24 16:19:52 +0530

    Fix indentation in todoWebApp.py

--- todoWebApp.py
+++ todoWebApp.py
@@ -17,5 +17,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 st.text_input(label="Enter a todo",placeholder="Enter a new todo",on_change=add_todo,key="new_todo")
-st.session_state
+
 
